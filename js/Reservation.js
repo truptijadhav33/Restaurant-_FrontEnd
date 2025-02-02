2525e5f2a3d587ea7404d3baec53a959e66d4746
Vidya Tambe <vidyatambe2004@gmail.com>
2025-02-02 14:42:49 +0530

    addition of prevent default

--- js/Reservation.js
+++ js/Reservation.js
@@ -3,3 +3,17 @@ console.dir(btn);
 btn.onsubmit=function(){
     alert(" Congratulations ! Your Table was book !!")
 }
+let btn=document.querySelector("button");
+console.dir(btn);
+
+btn.onclick=function(event){
+    event.preventDefault();
+    alert(" Congratulations !! Your Table was book !!")
+}
+let heading=document.querySelector("h1");
+setTimeout(()=>{
+    console.log("Welcome")
+},2000);
+btn.onsubmit=function(){
+    alert(" Congratulations ! Your Table was book !!")
+}
