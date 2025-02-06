ff69270deb5caa206ad7154e24381f283107b0c7
Vidya Tambe <vidyatambe2004@gmail.com>
2025-02-06 07:47:22 +0530

    add javascript

--- js/Reservation.js
+++ js/Reservation.js
@@ -1,19 +1,9 @@
 let btn=document.querySelector("button");
 console.dir(btn);
-btn.onsubmit=function(){
+btn.onsubmit=function(event){
+    event.preventDefault();
     alert(" Congratulations ! Your Table was book !!")
 }
-let btn=document.querySelector("button");
-console.dir(btn);
 
-btn.onclick=function(event){
-    event.preventDefault();
-    alert(" Congratulations !! Your Table was book !!")
-}
-let heading=document.querySelector("h1");
-setTimeout(()=>{
-    console.log("Welcome")
-},2000);
-btn.onsubmit=function(){
-    alert(" Congratulations ! Your Table was book !!")
-}
+
+
