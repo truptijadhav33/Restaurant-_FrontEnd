6d59b2d5aa46cb7e525c44c1bf258e9872db4869
Trupti Jadhav <jadhavtrupti33@gmail.com>
2025-02-16 16:55:34 +0530

    add:CSS

--- js/Reservation.js
+++
@@ -1,9 +0,0 @@
-let btn=document.querySelector("button");
-console.dir(btn);
-btn.onsubmit=function(event){
-    event.preventDefault();
-    alert(" Congratulations ! Your Table was book !!")
-}
-
-
-
